e587524f078c246cbf56c24ae5b1296d4d564214
Khayam Gondal <khayam.gondal@gmail.com>
2017-10-09 19:08:13 -0400

    new fields in prefs

--- src/main/java/edu/clemson/openflow/sos/utils/PrefsParser.java
+++ src/main/java/edu/clemson/openflow/sos/utils/PrefsParser.java
@@ -7,8 +7,10 @@ import java.io.Serializable;
 public class PrefsParser {
     @JsonProperty("agent-drivers")
     private String agentDrivers = "ptcp";
-    @JsonProperty("client-drivers")
-    private String clientDrivers = "netty";
+    @JsonProperty("client-sock-type")
+    private String clientSockType = "netty";
+    @JsonProperty("agent-sock-type")
+    private String agentSockType = "netty";
     @JsonProperty("max-clients")
     private int maxClientsSupported = 5;
 
@@ -16,33 +18,16 @@ public class PrefsParser {
 
     }
 
-    public PrefsParser(String agentDrivers,
-                       int maxClientsSupported,
-                       String clientDrivers) {
-        this.agentDrivers = agentDrivers;
-        this.maxClientsSupported = maxClientsSupported;
-        this.clientDrivers = clientDrivers;
-    }
-
-
-    public void setAgentDrivers(String agentDrivers) {
-        this.agentDrivers = agentDrivers;
-    }
-
-    public void setClientDrivers(String clientDrivers) {
-        this.clientDrivers = clientDrivers;
-    }
-
-    public void setMaxClientsSupported(int maxClientsSupported) {
-        this.maxClientsSupported = maxClientsSupported;
-    }
-
     public String getAgentDrivers() {
         return agentDrivers;
     }
 
-    public String getClientDrivers() {
-        return clientDrivers;
+    public String getClientSockType() {
+        return clientSockType;
+    }
+
+    public String getAgentSockType() {
+        return agentSockType;
     }
 
     public int getMaxClientsSupported() {
