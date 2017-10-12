b47bb12554fb9ed8fd50c47f531a0ad18f40f877
Khayam Gondal <khayam.gondal@gmail.com>
2017-10-12 15:20:33 -0400

    Simple Netty socket to drop packs

--- src/main/java/edu/clemson/openflow/sos/socks/netty/ClientSocketHandler.java
+++ src/main/java/edu/clemson/openflow/sos/socks/netty/ClientSocketHandler.java
@@ -2,8 +2,11 @@ package edu.clemson.openflow.sos.socks.netty;
 
 import com.sun.org.apache.regexp.internal.RE;
 import edu.clemson.openflow.sos.rest.RequestParser;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,11 +29,21 @@ public class ClientSocketHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // Discard the received data silently.
         InetSocketAddress socketAddress = (InetSocketAddress) ctx.channel().remoteAddress();
-        InetAddress inetaddress = socketAddress.getAddress();
-        log.info("New Client connected IP {} Port {}",
+        log.info("Got Message from {} at {}. Message is {}",
                 socketAddress.getHostName(),
-                socketAddress.getPort());
+                socketAddress.getPort(),
+                msg.toString());
+        ((ByteBuf) msg).release();
     }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
 }
